fedaa0f168d363e5ae37aadc74dc5c0d7ea44dbb
Michael <brandy@Mac.lan>
2026-05-22 12:49:47 -0500

    Bump app version to 2.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.lifelog.phone"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "2.0.1"
 
         buildConfigField("String", "PICOVOICE_ACCESS_KEY", "\"***REMOVED_PICOVOICE_KEY***==\"")
     }
